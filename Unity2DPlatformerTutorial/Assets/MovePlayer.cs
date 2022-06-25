c5fab12e45bc20d73e06ab74bec5eacccf66ca77
Ryan Swaby <ryan.swaby@gmail.com>
2022-06-25 02:17:03 -0400

    Finished jump animation and logic

--- Unity2DPlatformerTutorial/Assets/MovePlayer.cs
+++ Unity2DPlatformerTutorial/Assets/MovePlayer.cs
@@ -5,30 +5,67 @@ using UnityEngine;
 public class MovePlayer : MonoBehaviour
 {
     private Rigidbody2D body;
-    private SpriteRenderer renderer;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+   
 
     public float RunSpeed;
+    public float JumpHeight;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        renderer = GetComponent<SpriteRenderer>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
-          var horizontalInput = Input.GetAxis("Horizontal");
+        MoveLeftOrRight();
+
+        if (Input.GetKey(KeyCode.Space) && IsGrounded())
+        {
+            Jump();
+        }
+    }
+
+    void MoveLeftOrRight()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
 
         body.velocity = new Vector2(horizontalInput * RunSpeed, body.velocity.y);
 
-        if(horizontalInput > 0f)
+        if (horizontalInput > 0f)
         {
-            renderer.flipX = false;
+            spriteRenderer.flipX = false;
         }
-        else
+        else if (horizontalInput < 0f)
         {
-            renderer.flipX = true;
+            spriteRenderer.flipX = true;
         }
+
+        animator.SetBool("Run", horizontalInput != 0);
+    }
+
+    void Jump()
+    {
+        animator.SetBool("Grounded", false);
+        animator.SetTrigger("Jump");
+        body.velocity = new Vector2(body.velocity.x, JumpHeight);
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            animator.SetBool("Grounded", true);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return animator.GetBool("Grounded");
     }
 }
